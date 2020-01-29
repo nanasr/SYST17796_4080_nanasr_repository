9f15c110d3cc7bb295812150618dce06c0191715
nanasr <59941730+nanasr@users.noreply.github.com>
2020-01-28 22:36:26 -0500

    Update Funda1.java

--- src/Funda1.java
+++ src/Funda1.java
@@ -1,7 +1,11 @@
 
 public class Funda1 {
-
-    
+    /**
+    Name: nasreen
+    Author: Nasreen
+    Version:11
+    class: Softaware
+    */
     public static void main(String[] args) {
         String [] movies ={"pk", "dabbang", "good newz", "udta punjab"};
         for (String movie : movies){
